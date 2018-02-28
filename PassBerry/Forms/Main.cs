ed5092d10d6d2a5444c65620e074fc184ea9f153
Hendry <me@nap7.com>
2018-02-28 20:58:48 +0800

    PassBerry version 1.0.1.0 Add Notify Icon

--- PassBerry/Forms/Main.cs
+++ PassBerry/Forms/Main.cs
@@ -20,6 +20,7 @@
         {
             this.InitializeComponent();
             this.InitializeMainForm();
+            this.InitializeNotifyIcon();
         }
 
         private static List<RecordForDisplay> allDataCache;
@@ -28,13 +29,10 @@
         {
             var version = Assembly.GetExecutingAssembly().GetName().Version;
             this.Text = $"PassBerry {version.Major}.{version.Minor} - Press Shift + F2 to paste username and password";
-
             this.MinimumSize = new Size(700, 500);
-            //this.buttonAdd.Image = Properties.;
 
             var dataGrid = this.dataGridViewMain;
             dataGrid.ColumnHeadersDefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
-
             dataGrid.BackgroundColor = Color.White;
             dataGrid.BorderStyle = BorderStyle.None;
             dataGrid.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.None;
@@ -55,6 +53,32 @@
 
             allDataCache = ResourceProcessor.GetInstance().GetAll();
             this.LoadData(allDataCache);
+        }
+
+        private void InitializeNotifyIcon()
+        {
+            var notifyIcon = new NotifyIcon { Icon = this.Icon, Visible = true };
+            this.Closing += delegate
+            {
+                HotKeyHelper.UnregisterHotKey(this.Handle, 100);
+                HotKeyHelper.UnregisterHotKey(this.Handle, 101);
+            };
+
+            notifyIcon.MouseDown += (sender, e) =>
+            {
+                if (e.Button == MouseButtons.Left)
+                {
+                    this.Activate();
+                    this.Visible = true;
+                }
+                if (e.Button == MouseButtons.Right)
+                {
+                    if (MessageBox.Show("Are your sure to exit the PassBerry?", "Exit", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                    {
+                        this.Close();
+                    }
+                }
+            };
         }
 
         private void LoadData(List<RecordForDisplay> data)
@@ -118,7 +142,54 @@
             return result;
         }
 
+        #region Register Hotkey and reset Close and Min Window function
+
         private const int WmHotkey = 0x0312;
+        private const int WmSysCommand = 0x0112;
+        private const int ScMaxMize = 0xf030;
+        private const int ScMinMize = 0xf020;
+        private const int ScClose = 0xf060;
+
+        protected override void WndProc(ref Message message)
+        {
+            switch (message.Msg)
+            {
+                case WmSysCommand:
+                    var code = message.WParam.ToInt32();
+                    if (code == ScClose || code == ScMinMize)
+                    {
+                        this.Visible = false;
+                        // Must Prevent WndProc
+                        return;
+                    }
+                    // Others, such as SC_MAXMIZE must in WndProc.
+                    break;
+
+                case WmHotkey:
+                    switch (message.WParam.ToInt32())
+                    {
+                        // Shift + F2
+                        case 100:
+                            var text = this.GetCurrentPasteValue();
+                            if (!string.IsNullOrEmpty(text))
+                            {
+                                Clipboard.SetText(text);
+                                SendKeys.Send("^V");
+                                // Clipboard.Clear();
+                            }
+                            break;
+
+                        // Shift + F1
+                        case 101:
+                            this.Activate();
+                            this.Visible = true;
+                            break;
+                    }
+                    break;
+            }
+            base.WndProc(ref message);
+        }
+
         private bool IsCurrentPasteUsername = true;
 
         private string GetCurrentPasteValue()
@@ -140,45 +211,25 @@
             }
         }
 
-        protected override void WndProc(ref Message message)
-        {
-            switch (message.Msg)
-            {
-                case WmHotkey:
-                    switch (message.WParam.ToInt32())
-                    {
-                        // Shift + F2
-                        case 100:
-                            var text = this.GetCurrentPasteValue();
-                            if (!string.IsNullOrEmpty(text))
-                            {
-                                Clipboard.SetText(text);
-                                SendKeys.Send("^V");
-                                // Clipboard.Clear();
-                            }
-                            break;
-                    }
-                    break;
-            }
-            base.WndProc(ref message);
-        }
-
         private void CopyToClipBoard(object target)
         {
             if (target == null) return;
             Clipboard.SetDataObject(target, true);
         }
 
+        #endregion Register Hotkey and reset Close and Min Window function
+
+        #region Event Handler
+
         private void Main_Load(object sender, EventArgs e)
         {
             // Register Hotkey: Shift + F2
             // ID: 100
             HotKeyHelper.RegisterHotKey(this.Handle, 100, HotKeyHelper.KeyModifiers.Shift, Keys.F2);
-        }
-        
-        private void dataGridViewMain_RowLeave(object sender, DataGridViewCellEventArgs e)
-        {
-            this.IsCurrentPasteUsername = true;
+
+            // Register Hotkey: Shift + F1
+            // ID: 101
+            HotKeyHelper.RegisterHotKey(this.Handle, 101, HotKeyHelper.KeyModifiers.Shift, Keys.F1);
         }
 
         private void textBoxSearch_TextChanged(object sender, EventArgs e)
@@ -217,6 +268,11 @@
 
             // Copy current cell's value to clipboard
             this.CopyToClipBoard(targetCell.Value);
+        }
+
+        private void dataGridViewMain_RowLeave(object sender, DataGridViewCellEventArgs e)
+        {
+            this.IsCurrentPasteUsername = true;
         }
 
         private void buttonAdd_Click(object sender, EventArgs e)
@@ -259,5 +315,7 @@
         {
             new About().ShowDialog();
         }
+
+        #endregion Event Handler
     }
 }
